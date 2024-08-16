cb9be5c2e7bfde547b0b1fd97ba1312167cb7ddf
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-08-16 11:40:00 -0400

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', '23', 'Male', 'Brown', 'calm', TRUE),
+('Rowdy', '45', 'Male', 'Baige', 'rowdy', TRUE),
+('Tabitha', '25', 'Female', 'White', 'inquisitive', TRUE),
+('Sergeant Brown', '35', 'Male', 'Black', 'rigid', TRUE),
+('Melissa', '30', 'Female', 'light brown', 'fun', TRUE),
+('Grinch', '32', 'Male', 'Mix', 'naughty', TRUE),
+('Wendy', '29', 'Female', 'Black', 'studious', TRUE),
+(NULL, '32', 'Male', 'Mix', 'naughty', False);
+
